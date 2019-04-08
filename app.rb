3ca1fca8ac1cfe2319812c89d52747863e05cb61
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-04-08 21:15:47 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,8 +1,10 @@
+require "pry"
 require_relative 'config/environment'
 
 class App < Sinatra::Base
   
  get '/names' do 
+   binding.pry
    @names = Name.all 
    erb :'names/index.html.erb'
  end
