cadb842e3422bac87cca30a7d333fc6e7aaec066
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-04-08 21:32:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,8 +6,13 @@ class App < Sinatra::Base
  get '/name' do 
    "My name is Jennie"
  end
-  
-  
-  
+ 
+ get '/hometown' do 
+   "My hometown is Stode"
+ end
+ 
+ get '/favorite-song' do 
+   "My favorite song is Natural Woman"
+ end
   
 end
